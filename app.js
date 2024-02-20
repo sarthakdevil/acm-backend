e45b0dc1dce334825c920461f3eb516ca0c5753b
Sarthakdevil <Sarthakrajesh2005@gmail.com>
2024-02-20 17:37:33 +0530

    implemented cors

--- app.js
+++ app.js
@@ -5,11 +5,18 @@ import dotenv from 'dotenv';
 import errorHandler from './error/error.utils.js';
 import  router from './router/auth.router.js';
 import rateLimit from 'express-rate-limit';
+
 dotenv.config();
 const app = express();
 
 app.use(morgan('dev'));
 app.use(express.json());
+app.use(cors())
+
+const corsOptions = {
+    origin: `http://localhost:${process.env.PORT}`, // Allow requests from example.com
+    methods: 'GET,POST', // Allow only GET and POST requests
+  };
 
 const limiter = rateLimit({
     windowMs: 15 * 60 * 1000, // 15 minutes
