f76e63904ec2984bf13ac0eca93d7f3e7516d078
Sarthakdevil <Sarthakrajesh2005@gmail.com>
2024-02-26 23:27:27 +0530

    made viewer counter and members controlller

--- app.js
+++ app.js
@@ -6,7 +6,9 @@ import errorHandler from './utils/error.utils.js';
 import  router from './router/auth.router.js';
 import rateLimit from 'express-rate-limit';
 import blogrouter from './router/blog.router.js';
-
+import memberrouter from './router/acmmembers.router.js';
+import { PrismaClient } from '@prisma/client';
+const prisma = new  PrismaClient();
 dotenv.config();
 const app = express();
 
@@ -37,11 +39,29 @@ app.get('/ping',
 });
 app.use('/api',limiter,router)
 app.use('/api',limiter,blogrouter)
+app.use('/api',limiter,memberrouter)
 
-app.get('*',(req,res)=>{
-    res.status(404).send('404 not fond')
-})
-
+app.get('*', async (req, res) => {
+    try {
+      // Increment the visitor count
+      await prisma.visitorCount.update({
+        where: { id: 1 }, // Assuming the visitor count is stored with ID 1
+        data: { count: { increment: 1 } }
+      });
+  
+      // Retrieve the updated visitor count
+      const updatedVisitorCount = await prisma.visitorCount.findUnique({ where: { id: 1 } });
+  
+      console.log(`Visitor Count: ${updatedVisitorCount.count}`);
+  
+      // Send the response
+      res.status(404).send('404 Not Found');
+    } catch (error) {
+      console.error('Error updating visitor count:', error);
+      res.status(500).send('Internal Server Error');
+    }
+  });
+  
 app.use(errorHandler);
 
 export default app;
